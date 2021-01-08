1ce7baa9a7d71a4ade1828f48451f48bc6d5b2cd
Satyam Saxena <satyam2saxena@gmail.com>
2021-01-08 11:11:43 +0530

    Add student class and its methods for getting the average score

---
+++ src/main/Student.java
@@ -0,0 +1,49 @@
+public class Student {
+    private String name;
+//    private int roll;
+    private int mathScore;
+    private int englishScore;
+    private int csScore;
+    private final static int COURSE_LIST_SIZE = 3;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMathScore() {
+        return mathScore;
+    }
+
+    public void setMathScore(int mathScore) {
+        this.mathScore = mathScore;
+    }
+
+    public int getEnglishScore() {
+        return englishScore;
+    }
+
+    public void setEnglishScore(int englishScore) {
+        this.englishScore = englishScore;
+    }
+
+    public int getCsScore() {
+        return csScore;
+    }
+
+    public void setCsScore(int csScore) {
+        this.csScore = csScore;
+    }
+
+    public int getTotalScore()
+    {
+        return this.csScore + this.mathScore + this.englishScore;
+    }
+    public double getAvgScore(){
+        double totalScore = getTotalScore();
+        return totalScore/ COURSE_LIST_SIZE;
+    }
+}
